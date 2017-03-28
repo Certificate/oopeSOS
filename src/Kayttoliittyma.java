3d7b545e7919d1427510de94c4fbe23050ae55cf
java <valtteri.vuori@live.fi>
2017-03-29 01:40:20 +0300

    Vaihdettiin vakioidut komennot komentotulkin niskaan

--- src/Kayttoliittyma.java
+++ src/Kayttoliittyma.java
@@ -9,46 +9,6 @@ import apulaiset.In;
 
 public class Kayttoliittyma {
 
-    private static final String POLKU = "/>";
-
-    private static final String MAKEDIR = "md";
-    // luo parametrina annetulla nimellä alihakemiston nykyiseen
-    // hakemistoon, jos samannimistä alihakemisto ei jo ole.
-
-    private static final String MAKEFIL = "mf";
-    // luo annetun nimisen ja kokoisen tiedoston nykyiseen
-    // hakemistoon, jos samannimistä tiedostoa ei jo ole.
-
-    private static final String CHANGEDIR = "cd";
-    // asettaa yöhakemistoksi joko yli- tai alihakemiston, jos voidaan siirtyä.
-    // Ylihakemistoon viitataan kahdella pisteellä.
-    // Ilman parametria palataan juureen.
-
-    private static final String LIST = "ls";
-    // listaa näytölle annetun nimisen tiedoston tai hakemiston
-    // tiedot, jos listauksen kohde löytyy nykyisestä hakemistosta.
-    // Listaa nykyisen hakemiston sisällön, jos parametria ei anneta.
-
-    private static final String FIND = "find";
-    // listaa hakemiston rekursiivisesti esijärjestyksessä.
-
-    private static final String REMOVE = "rm";
-    // poistaa varmistamatta tiedoston tai hakemiston, jos sellainen on
-    // nykyhakemistossa
-
-    private static final String COPY = "cp";
-    // kopioi tiedoston annetun nimiseksi uudeksi
-    // tiedostoksi, jos nimellä löydetään tiedosto nykyhakemistosta
-    // ja hakemistossa ei ole vielä uuden nimistä tiedostoa.
-
-    private static final String MOVE = "mv";
-    // nimeää tiedoston annetun nimiseksi uudeksi
-    // tiedostoksi, jos nimellä löydetään tiedosto nykyhakemistosta
-    // ja hakemistossa ei ole vielä uuden nimistä tiedostoa.
-
-    private static final String EXIT = "exit";
-    // lopetaa ohjelman.
-
     private Hakemisto dir = new Hakemisto();
     private Komentotulkki tulkki = new Komentotulkki();
 
